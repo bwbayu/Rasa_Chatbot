fd3f55b1fa82c4071e707feaf9e31098243d11da
bwbayu <bwbayuuu@gmail.com>
2024-02-19 20:31:57 +0700

    action button (tipe kamar)

--- thirdproject/actions/actions.py
+++ thirdproject/actions/actions.py
@@ -3,25 +3,26 @@
 #
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
+# rasa run actions -v
+from typing import Any, Text, Dict, List
+import random
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
 
+class ActionRoomType(Action):
 
-# This is a simple example for a custom action which utters "Hello World!"
+    def name(self) -> Text:
+        return "action_select_room_type"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        room_types = ["kamar tunggal", "kamar ganda", "kamar suite", "kamar keluarga"]
+
+        chosen_room_type = random.choice(room_types)
+
+        dispatcher.utter_message(text=f"Saya ingin {chosen_room_type}")
+
+        return [SlotSet("chosen_room_type", chosen_room_type)]
